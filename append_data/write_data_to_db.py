5e76c0091c11c08ff85dc8e57f20647ec83c0f0f
chuangw46 <30398455+chuangw46@users.noreply.github.com>
2020-05-14 19:53:35 +1000

    minor1

--- append_data/write_data_to_db.py
+++ append_data/write_data_to_db.py
@@ -4,17 +4,16 @@ from couchdb import Server
 fileName = 'states.geojson'
 
 server = Server('http://admin:password@172.26.131.49:5984//')
-db = server['gov_geo_data']
+# db = server['test_chuangw_du']
 
-
-def write_to_db(filename):
-    with open(filename, 'r', encoding='utf-8') as file_obj:
-        data = json.load(file_obj)
-        data.update(_id="AU_States_Geo_Data")
-        db.save(data)
-
-
-write_to_db(fileName)
+# def write_to_db(filename):
+#     with open(filename, 'r', encoding='utf-8') as file_obj:
+#         data = json.load(file_obj)
+#         data.update(_id="AU_States_Geo_Data")
+#         db.save(data)
+#
+#
+# write_to_db(fileName)
 
 
 # server = Server('http://admin:password@172.26.131.49:5984//')
