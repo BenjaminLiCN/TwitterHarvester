803bbcd7bb550a6e0cd18bd4db21d186d7abe179
chuangw46 <30398455+chuangw46@users.noreply.github.com>
2020-05-14 16:38:41 +1000

    reformat code

--- write_data_to_db.py
+++ write_data_to_db.py
@@ -15,3 +15,33 @@ def write_to_db(filename):
 
 
 write_to_db(fileName)
+
+
+# server = Server('http://admin:password@172.26.131.49:5984//')
+# db = server['test_chuangw']
+#
+# doc = {
+#   "_id": "1251734552747229184",
+#   "doc": {
+#     "User_name": "1JYK1",
+#     "Time": "2020-04-19T04:49:08",
+#     "Tweets": "Finished product in time for lunch. #isobaking @ Cowaramup, Western Australia https://t.co/m4CJJtOink",
+#     "Hashtag": [
+#       "isobaking"
+#     ],
+#     "Length": 101,
+#     "Likes": 0,
+#     "Retweet": 0,
+#     "Location": "Sydney New South Wales ",
+#     "Coordinates": {
+#       "type": "Point",
+#       "coordinates": [
+#         115.10287,
+#         -33.85094
+#       ]
+#     }
+#   }
+# }
+#
+#
+# db.save(doc)
