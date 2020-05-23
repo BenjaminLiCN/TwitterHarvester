fc82c377cbe0d489b5c0e433e9dae1e82e5a2c2b
wiwilin <katherine.linw@gmail.com>
2020-05-23 13:30:56 +1000

    update build

---
+++ Front_end/build/precache-manifest.160639ced39cb377d3778873efda9845.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc93ad0e3754c1c009a6f18595a9ff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81b496c837f3f87c8763",
+    "url": "/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "bb916b54e0e445ac2a24",
+    "url": "/static/css/main.890d0f4b.chunk.css"
+  },
+  {
+    "revision": "81b496c837f3f87c8763",
+    "url": "/static/js/2.7abb4b9e.chunk.js"
+  },
+  {
+    "revision": "257c8c5a628c6334d25a089c51fcffa1",
+    "url": "/static/js/2.7abb4b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb916b54e0e445ac2a24",
+    "url": "/static/js/main.47e6262b.chunk.js"
+  },
+  {
+    "revision": "64f79a916d193f5a9e41",
+    "url": "/static/js/runtime-main.1ae96f22.js"
+  },
+  {
+    "revision": "a48dc7d8a27cc71f9ffcc1c643eeff23",
+    "url": "/static/media/benjieming.a48dc7d8.jpg"
+  },
+  {
+    "revision": "5d127d75aa82747cefe4830d99002cb0",
+    "url": "/static/media/wang.5d127d75.jpg"
+  },
+  {
+    "revision": "be9cefa4dbe911aeec0e4a86f3fe52a2",
+    "url": "/static/media/wei.be9cefa4.jpg"
+  },
+  {
+    "revision": "c6b56689c3391157add7f5d212f3bb37",
+    "url": "/static/media/yangyang.c6b56689.jpg"
+  },
+  {
+    "revision": "4a7638a649d1a65b8bdb0f5cc69ae10e",
+    "url": "/static/media/zhang.4a7638a6.jpg"
+  }
+]);
