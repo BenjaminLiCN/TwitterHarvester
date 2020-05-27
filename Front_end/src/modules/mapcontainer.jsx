cb288e929b10d3b3462092e53ecb74644dce784a
wiwilin <katherine.linw@gmail.com>
2020-05-27 11:16:49 +1000

    add comment

--- Front_end/src/modules/mapcontainer.jsx
+++ Front_end/src/modules/mapcontainer.jsx
@@ -1,3 +1,13 @@
+/*@author: Team18(member details are as follows)
+
+Name(Firstname Surname)	|	Username	|	StudentID	|	City
+---------------------------------------------------------------------
+    Chuang Wang				|	chuangw		|	791793		| Melbourne
+Honglong Zhang			|	honglongz	|	985262		| Melbourne
+Jingyi Li				|	jili		|	961543		| Melbourne
+Wei Lin					|	wlin8		|	885536		| Melbourne
+Yangyang Hu				|	Yangyangh1	|	978954		| Melbourne*/
+
 import React, {Component, useState} from 'react';
 import ReactDOM from "react-dom";
 import ReactDOMServer from 'react-dom/server';
